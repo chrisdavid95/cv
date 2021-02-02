6458f45fb8385e67e220401cb7233f6cb2b13148
chrisdavid95 <david.c.fr95@gmail.com>
2021-02-02 12:20:23 +0100

    Nouveau CV version 1.0

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CV de Christian DAVID</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"
+		integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<!-- TITRE + OBJET -->
+	<header class="container">
+		<div id="titre">
+			<h1>Développeur Web en Alternance</h1>
+			<h2>Contrat de professionnalisation</h2>
+			<p>
+				Semaine A : 4 jours / 1 jour<br/>
+				&<br/>
+				Semaine B : 3 jours / 2 jours
+			</p>
+		</div>
+	</header>
+
+	<main class="container">
+
+		<div>
+			<section id="profil">
+				<h2>Christian DAVID</h2>
+				<div>
+					<img alt="photo de christian david" src="photo/chris.jpg" id="moi">
+					<p>
+						<ul>
+							<li><a href="mailto:david.c.fr95@gmail.com">david.c.fr95@gmail.com</a></li>
+							<li><a href="tel:+33779821059">0779821059</a></li>
+							<li>30 ans</li>
+							<li>Véhiculé</li>
+							<li><a href="https://www.linkedin.com/in/christian-david-06158919b/">Linkedin</a></li>
+						</ul>
+					</p>
+				</div>
+			</section>
+
+			<section id="formations">
+				<h2>Formations</h2>
+					<p>Développeur d'applicaiton - PHP / Symfony (bac +3/4) :</p>
+						<ul>
+							<li><time datetime="2021-02-01">2021 - 2023</time></li>
+							<li>PHP orienté objet</li>
+							<li>Bases de données MySQL</li>
+							<li>Symfony 4</li>
+							<li>API REST</li>
+						</ul>
+					<p>Intégration Web avec HTML5 & CSS3 (réussie) :</p>
+						<ul>
+							<li><time datetime="2021-02-01"><a href="photo/certificat_HTML5_&_CSS3.pdf">08/2020 - 11/2020</a></time></li>
+							<li>Conformité W3C</li>
+							<li>Définir/Intégrer à partir d'une maquette</li>
+							<li>Affichage responsive</li>
+							<li>Versionning Git & GitHub</li>
+						</ul>
+			</section>
+		</div>
+
+		<div>
+			<section id="technologies">
+				<h2>Technologies</h2>
+				<p>Connu :<br>
+					<i class="fab fa-html5"></i>
+					<i class="fab fa-css3-alt"></i>
+					<i class="fab fa-sass"></i>
+					<i class="fab fa-git"></i>
+					<i class="fab fa-js-square"></i>
+					<i class="fab fa-wordpress"></i>
+					<i class="fab fa-php"></i>
+					<i class="fab fa-node-js"></i>
+				</p>
+				<h2>Réalisations</h2>
+				<p>
+					<a href="https://chrisdavid95.github.io/projet-reservia/"><i class="fas fa-paperclip"></i></a>Projet Réservia<br>
+					<a href="https://chrisdavid95.github.io/"><i class="fas fa-paperclip"></i></a>Ancien CV
+				</p>
+			</section>
+
+			<section id="soft_skills">
+				<h2>Soft Skills</h2>
+				<ul>
+					<li>Rapidité de compréhension</li>
+					<li>Travail en équipe</li>
+					<li>Résoudre des problèmes</li>
+					<li>Autonome</li>
+					<li>Gestion de l'organisation</li>
+				</ul>
+			</section>
+		</div>
+
+	</main>
+
+	<footer class="container">
+
+		<div>
+			<section id="langues">
+				<h2>Compétences linguistiques</h2>
+				<ul>
+					<li>Anglais courant</li>
+					<li>Espagnol courant</li>
+				</ul>
+			</section>
+			<section id="avantages">
+				<h2>Les avantages</h2>
+				<p><i class="far fa-lightbulb"></i><strong>2000 €</strong> de mon salaire annuel pris en charge par le Pôle emploi grâce à l'<strong>AFE</strong> (Aide Forfaitaire à l'Employeur).</p>
+			</section>
+		</div>
+
+		<div id="bilan">
+			<p><i class="far fa-hand-point-right"></i>En résumé, vous avez potentiellement un future collaborateur <strong>surmotivé</strong> et désireux de se forgé une réelle expérience.</p>
+		</div>
+		
+	</footer>
+</body>
+</html>
